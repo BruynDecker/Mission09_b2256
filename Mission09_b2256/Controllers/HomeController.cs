ca0533b2c3ab56405e350c89d540bbab2fb25ef8
bruyndecker <bruyn.decker@byu.edu>
2023-03-16 14:34:07 -0600

    Notes (commenting)

--- Mission09_b2256/Controllers/HomeController.cs
+++ Mission09_b2256/Controllers/HomeController.cs
@@ -12,21 +12,24 @@ namespace Mission09_b2256.Controllers
 {
     public class HomeController : Controller
     {
-        private IBookRepository repo;
+        private IBookRepository repo; // Declare an instance variable for the repository.
 
-        public HomeController (IBookRepository temp)
+        // Create a constructor that takes an IBookRepository parameter and assigns it to the instance variable.
+        public HomeController(IBookRepository temp)
         {
             repo = temp;
         }
 
-        public IActionResult Index(string cateGory, int pagenum =1)
+        // Define an action method called "Index" that accepts a string parameter "cateGory" and an optional integer parameter "pagenum".
+        public IActionResult Index(string cateGory, int pagenum = 1)
         {
-            int pageSize = 5;
+            int pageSize = 5; // Set the number of items to display per page.
 
+            // Create a ProjectsViewModel object that contains a subset of the books in the repository based on the specified category and page number.
             var x = new ProjectsViewModel
             {
-                Book = repo.Book
-                .Where(b => b.Category == cateGory || cateGory == null)
+                Book = repo.Book // Get all books from the repository.
+                .Where(b => b.Category == cateGory || cateGory == null) // Filter the books based on the category parameter (if specified).
                 .OrderBy(b => b.Title)
                 .Skip(((pagenum) - 1) * pageSize)
                 .Take(pageSize),
--- Mission09_b2256/Controllers/PurchaseController.cs
+++ Mission09_b2256/Controllers/PurchaseController.cs
@@ -9,39 +9,46 @@ namespace Mission09_b2256.Controllers
 {
     public class PurchaseController : Controller
     {
-        private IPurchaseRepository repo { get; set; }
-        private Basket basket { get; set; }
+        private IPurchaseRepository repo { get; set; } // Dependency injection of IPurchaseRepository
+        private Basket basket { get; set; } // Dependency injection of Basket
+
         public PurchaseController(IPurchaseRepository temp, Basket b)
         {
             repo = temp;
             basket = b;
         }
 
-        [HttpGet]
+        [HttpGet] // HttpGet attribute indicates that this method handles GET requests
         public IActionResult Checkout()
         {
-            return View(new Cart());
+            return View(new Cart()); // Displays Checkout view with a new instance of Cart object
         }
-        [HttpPost]
+
+        [HttpPost] // HttpPost attribute indicates that this method handles POST requests
         public IActionResult Checkout(Cart cart)
         {
-            if (basket.Items.Count() == 0)
+            if (basket.Items.Count() == 0) // Check if the basket is empty
             {
-                ModelState.AddModelError("", "Sorry, your basket's empty!");
-
+                ModelState.AddModelError("", "Sorry, your basket's empty!"); // Add a model error message
             }
-            if (ModelState.IsValid)
+
+            if (ModelState.IsValid) // If model state is valid, proceed with the purchase
             {
-                cart.Lines = basket.Items.ToArray();
-                repo.SavePurchase(cart);
-                basket.ClearBasket();
+                cart.Lines = basket.Items.ToArray(); // Assign items in the basket to the cart
+                repo.SavePurchase(cart); // Save the purchase to the repository
+                basket.ClearBasket(); // Clear the basket
 
-                return RedirectToPage("/Completion");
+                return RedirectToPage("/Completion"); // Redirect to the Completion page
             }
-            else
+            else // If model state is invalid, redisplay the Checkout view
             {
                 return View();
             }
         }
     }
 }
+
+//This is a PurchaseController class that handles purchase-related requests. It has two action methods:
+
+//1. `Checkout` -a GET method that displays a view to the user with a new instance of `Cart` object.
+//2. `Checkout` -a POST method that processes the form data submitted by the user.
